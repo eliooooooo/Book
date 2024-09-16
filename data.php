3fb2b82633a5098ff677b4945b591df7264cfb59
Eliott <eliott.burkle@gmail.com>
2024-09-16 19:28:32 +0200

    Feat: add data and twig template

---
+++ data.php
@@ -0,0 +1,70 @@
+<?php
+$data = [
+    [
+        'tip' => 'Extension',
+        'title' => 'Write My Commits',
+        'plot' => 'Write My Commits est une extension VsCode permettant de générer vos messages et noms de commits selon la convention "Conventional Commits".',
+        'git' => 'https://github.com/eliooooooo/WriteMyCommits',
+        'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.writemycommits'
+    ],
+    [
+        'tip' => 'Script',
+        'title' => 'Doc My Files',
+        'plot' => 'Doc My Files est un script permettant de générer un README personnalisé en utilisant l\'API OpenAI.',
+        'git' => 'https://github.com/eliooooooo/DocMyFiles',
+        'link' => 'https://github.com/eliooooooo/DocMyFiles'
+    ],
+    [
+        'tip' => 'Web App',
+        'title' => 'Chart My Time',
+        'plot' => 'Chart My Time est une application web pour le suivi de votre temps, développée sur mon temps libre, à partir de zéro et sans frameworks.',
+        'git' => 'https://github.com/eliooooooo/ChartMyTime',
+        'link' => 'https://elioooooo.fr/chart-my-time/'
+    ],
+    [
+        'tip' => 'Full-Stack',
+        'title' => 'Custom CMS',
+        'plot' => 'Custom CMS est un système de gestion de contenu développé en PHP, sans framework. Il a permis de créer un site de contenu journalistique.',
+        'git' => 'https://github.com/eliooooooo/Custom-CMS',
+        'link' => 'https://www.cuej.info/mini-sites/sauvage/'
+    ],
+    [
+        'tip' => 'Full-Stack',
+        'title' => 'The Shoes Spot',
+        'plot' => 'The Shoes Spot est un site e-commerce fictif développé en PHP, sans framework. Pendant ce projet j\'ai pu découvrir la gestion d\'un site basé sur une base de donnée.',
+        'git' => 'https://github.com/eliooooooo/TheShoesSpot',
+    ],  
+    [
+        'tip' => 'Package',
+        'title' => 'Starter Tailwind-Sass',
+        'plot' => 'Ce package permet de démarer un projet en utilisant Tailwind CSS et Sass et proposant une compilation. Il peut être combiné avec d\'autres technologies.',
+        'git' => 'https://github.com/eliooooooo/Starter-tailwind-sass',
+        'link' => 'https://elioooooo.fr/Starter-tailwind-sass/'
+    ],
+    [
+        'tip' => 'Front-End',
+        'title' => 'Javascript games',
+        'plot' => 'Lors de ce projet j\'ai pu découvrir et pratiquer les bases des animations Javascript pour développer plusieurs mini-jeux.',
+        'git' => 'https://github.com/eliooooooo/Js-Animation',
+    ],
+    [
+        'tip' => 'Documentation',
+        'title' => 'Personnal GSAP Doc',
+        'plot' => 'Ce projet provient de mon apprentissage de la librairie GSAP. J\'ai créé une documentation personnelle pour m\'aider à mémoriser son fonctionnement.',
+        'git' => 'https://github.com/eliooooooo/Integration-GSAP',
+    ],  
+    [
+        'tip' => 'Front-End',
+        'title' => 'Le Louvre V.2',
+        'plot' => 'Intégration d\'un site en utilisant le moteur de template twig. J\'ai pu découvrir et pratiquer la gestion d\'un site avec un système de template..',
+        'git' => 'https://github.com/eliooooooo/Louvre-v2',
+        'link' => 'https://elioooooo.fr/Louvre-v2/'
+    ],  
+    [
+        'tip' => 'Front-End',
+        'title' => 'Le Louvre V.1',
+        'plot' => 'Mon premier site web, j\'ai pu découvrir et pratiquer les bases de l\'intégration web ainsi que découvrir les différents outils.',
+        'git' => 'https://github.com/eliooooooo/Louvre-v1',
+        'link' => 'https://elioooooo.fr/Louvre-v1/'
+    ]    
+];
