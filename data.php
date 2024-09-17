69d9c3911a49e3677fe8dbe3a13add439536363a
Eliott <eliott.burkle@gmail.com>
2024-09-17 14:56:02 +0200

    Feat: update projects

--- data.php
+++ data.php
@@ -1,20 +1,6 @@
 <?php
 $data = [
     [
-        'tip' => 'Extension',
-        'title' => 'Write My Commits',
-        'plot' => 'Write My Commits est une extension VsCode permettant de générer vos messages et noms de commits selon la convention "Conventional Commits".',
-        'git' => 'https://github.com/eliooooooo/WriteMyCommits',
-        'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.writemycommits'
-    ],
-    [
-        'tip' => 'Script',
-        'title' => 'Doc My Files',
-        'plot' => 'Doc My Files est un script permettant de générer un README personnalisé en utilisant l\'API OpenAI.',
-        'git' => 'https://github.com/eliooooooo/DocMyFiles',
-        'link' => 'https://github.com/eliooooooo/DocMyFiles'
-    ],
-    [
         'tip' => 'Web App',
         'title' => 'Chart My Time',
         'plot' => 'Chart My Time est une application web pour le suivi de votre temps, développée sur mon temps libre, à partir de zéro et sans frameworks.',
@@ -27,6 +13,47 @@ $data = [
         'plot' => 'Custom CMS est un système de gestion de contenu développé en PHP, sans framework. Il a permis de créer un site de contenu journalistique.',
         'git' => 'https://github.com/eliooooooo/Custom-CMS',
         'link' => 'https://www.cuej.info/mini-sites/sauvage/'
+    ],
+    [
+        'tip' => 'Full-Stack',
+        'title' => 'Site du LCO',
+        'plot' => 'Ce site est basé sur un thème Wordpress personnalisé crée pour l\'occasion et selon les critères de l\'association.',
+        'git' => 'https://github.com/eliooooooo/LCO-custom-wp',
+        'link' => 'https://lcoberschaeffolsheim.fr/'
+    ],
+    [
+        'tip' => 'Full-Stack',
+        'title' => 'BundleBuddy',
+        'plot' => 'BundleBuddy est une appliquation web développée en Symfony permettant aux développeurs de créer leurs packages npm et les sauvegarder',
+        'git' => 'https://github.com/eliooooooo/BundleBuddy',
+        'link' => 'https://bundlebuddy.burkle.etu.mmi-unistra.fr/'
+    ],
+    [
+        'tip' => 'Script',
+        'title' => 'Doc My Files',
+        'plot' => 'Doc My Files est un script permettant de générer un README personnalisé en utilisant l\'API OpenAI.',
+        'git' => 'https://github.com/eliooooooo/DocMyFiles',
+        'link' => 'https://github.com/eliooooooo/DocMyFiles'
+    ],
+    [
+        'tip' => 'Extension',
+        'title' => 'QuickReplace',
+        'plot' => 'QuickReplace permet de créer des suggestions de remplacement de texte, basées sur vos propres strings et patterns.',
+        'git' => 'https://github.com/eliooooooo/QuickReplace',
+        'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.quickreplace'
+    ],
+    [
+        'tip' => 'Extension',
+        'title' => 'Write My Commits',
+        'plot' => 'Write My Commits est une extension VsCode permettant de générer vos messages et noms de commits selon la convention "Conventional Commits".',
+        'git' => 'https://github.com/eliooooooo/WriteMyCommits',
+        'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.writemycommits'
+    ],
+    [
+        'tip' => 'Unity',
+        'title' => 'Find the remote',
+        'plot' => 'Find the remote est un jeu crée en équipe sur unity. Ce projet à permis de travailler la modélisation 3D ainsi que la prise en main de unity.',
+        'git' => 'https://github.com/eliooooooo/FindTheRemote'
     ],
     [
         'tip' => 'Full-Stack',
@@ -46,6 +73,7 @@ $data = [
         'title' => 'Javascript games',
         'plot' => 'Lors de ce projet j\'ai pu découvrir et pratiquer les bases des animations Javascript pour développer plusieurs mini-jeux.',
         'git' => 'https://github.com/eliooooooo/Js-Animation',
+        'link' => 'https://elioooooo.fr/js-animation/'
     ],
     [
         'tip' => 'Documentation',
