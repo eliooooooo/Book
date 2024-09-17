ae3a74eb53a1787df2d3ee2e64514792b2bf99f4
Eliott <eliott.burkle@gmail.com>
2024-09-17 11:41:54 +0200

    Feat: Create templates directory

--- src/twig.php
+++ src/twig.php
@@ -9,13 +9,13 @@ require_once('../vendor/autoload.php');
 // Fonction qui permet d'initialiser Twig en fixant le dossier des modèles
 function init_twig()
 {
-	// Indique le répertoire ou sont placés les modèles (templates)
-	$loader = new FilesystemLoader('../templates');
+    // Indique le répertoire ou sont placés les modèles (templates)
+    $loader = new FilesystemLoader('../templates');
 
-	// Crée un nouveau moteur Twig
-	$twig = new Environment($loader, ['debug' => true]);
-	$twig->addExtension(new DebugExtension());
+    // Crée un nouveau moteur Twig
+    $twig = new Environment($loader, ['debug' => true]);
+    $twig->addExtension(new DebugExtension());
 
-	// Renvoie le moteur
-	return $twig;
+    // Renvoie le moteur
+    return $twig;
 }
