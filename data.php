aee48c8cd0700575b04db11f4e3079253f186bb1
eliooooooo <eliott.burkle@gmail.com>
2025-06-17 09:45:11 +0200

    Data: Remove older projects

--- data.php
+++ data.php
@@ -1,12 +1,6 @@
 <?php
 $data = [
     [
-        'tip' => 'Full-Stack',
-        'title' => 'Slack application',
-        'plot' => 'Une application permettant de recenser les spots de plusieurs disciplines (slackline, highline, tree climbing). Basée sur les fonctionnalités de Firebase.',
-        'git' => 'https://github.com/eliooooooo/slack'
-    ],
-    [
         'tip' => 'Front-End',
         'title' => 'Site monitoring Dashboard',
         'plot' => 'Track My Site est un tableau de bord de surveillance de site web développé en VueJS. Il permet de surveiller les performances de votre site.',
@@ -45,23 +39,16 @@ $data = [
         'git' => 'https://github.com/eliooooooo/Mellow-Clo-Drupal-Reproduction'
     ],
     [
-        'tip' => 'Full-Stack',
-        'title' => 'Custom CMS',
-        'plot' => 'Custom CMS est un système de gestion de contenu développé en PHP, sans framework. Il a permis de créer un site de contenu journalistique.',
-        'git' => 'https://github.com/eliooooooo/Custom-CMS',
-    ],
-    [
-        'tip' => 'Full-Stack',
+        'tip' => 'Web App',
         'title' => 'EasyGest',
         'plot' => 'EasyGest est une application web permettant de gérer une micro-entreprise de manière simplifiée. Développée en Laravel à l\'aide du framework Filament.',
         'git' => 'https://github.com/eliooooooo/EasyGest'
     ],
     [
-        'tip' => 'IA',
-        'title' => 'Climbing Related Model',
-        'plot' => 'Climbing Related Model est un modèle de machine learning développé en Python pour définir ou non si une requête utilisateur est liée à l\'escalade.',
-        'git' => 'https://github.com/eliooooooo/ClimbingRelatedModel',
-        'link' => 'https://www.climbing-model.elioooooo.fr/'
+        'tip' => 'Full-Stack',
+        'title' => 'Custom CMS',
+        'plot' => 'Custom CMS est un système de gestion de contenu développé en PHP, sans framework. Il a permis de créer un site de contenu journalistique.',
+        'git' => 'https://github.com/eliooooooo/Custom-CMS',
     ],
     [
         'tip' => 'Front-End',
@@ -85,24 +72,10 @@ $data = [
     ],
     [
         'tip' => 'Extension',
-        'title' => 'QuickReplace',
-        'plot' => 'QuickReplace permet de créer des suggestions de remplacement de texte, basées sur vos propres strings et patterns.',
-        'git' => 'https://github.com/eliooooooo/QuickReplace',
-        'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.quickreplace'
-    ],
-    [
-        'tip' => 'Extension',
         'title' => 'Write My Commits',
         'plot' => 'Write My Commits est une extension VsCode permettant de générer vos messages et noms de commits selon la convention "Conventional Commits".',
         'git' => 'https://github.com/eliooooooo/WriteMyCommits',
         'link' => 'https://marketplace.visualstudio.com/items?itemName=BURKLEEliott.writemycommits'
-    ],
-    [
-        'tip' => 'Unity',
-        'title' => 'Find the remote',
-        'plot' => 'Find the remote est un jeu crée en équipe sur unity. Ce projet à permis de travailler la modélisation 3D ainsi que la prise en main de unity.',
-        'git' => 'https://github.com/eliooooooo/FindTheRemote',
-        'link' => 'https://gamejolt.net/?token=c4doHxcqXmM6RYZUdRZoHWCP7QnbQT'
     ],
     [
         'tip' => 'Full-Stack',
@@ -110,17 +83,5 @@ $data = [
         'plot' => 'The Shoes Spot est un site e-commerce fictif développé en PHP, sans framework. Pendant ce projet j\'ai pu découvrir la gestion d\'un site basé sur une base de donnée.',
         'git' => 'https://github.com/eliooooooo/TheShoesSpot',
         'link' => 'https://shoes.elioooooo.fr/'
-    ],  
-    [
-        'tip' => 'Package',
-        'title' => 'Starter Tailwind-Sass',
-        'plot' => 'Ce package permet de démarer un projet en utilisant Tailwind CSS et Sass et proposant une compilation. Il peut être combiné avec d\'autres technologies.',
-        'git' => 'https://github.com/eliooooooo/Starter-tailwind-sass',
-    ],
-    [
-        'tip' => 'Front-End',
-        'title' => 'Javascript games',
-        'plot' => 'Lors de ce projet j\'ai pu découvrir et pratiquer les bases des animations Javascript pour développer plusieurs mini-jeux.',
-        'git' => 'https://github.com/eliooooooo/Js-Animation',
     ]
 ];
--- index.php
+++ index.php
@@ -95,7 +95,7 @@
       </nav>
     </div>
       <div class="infosContainer absolute bottom-10 right-10 text-black dark:text-white max-w-[220px] text-lg">
-        <p>Actuellement étudiant et alternant dans la formation BUT MMI à l'IUT de Haguenau. Développeur Front-end en alternance, je développe aussi sur mon temps libre des projets webs, <span class="text-orange-500 dark:text-yellow-400" >front et back-end</span>.</p>
+        <p>Actuellement étudiant et alternant dans le développement web, j'aime apprendre et m'inition à de nouvelles pratiques, de nature "touche à tout" je pratique le développement <span class="text-orange-500 dark:text-yellow-400" >Full Stack</span>.</p>
       </div>
       <div class="projetsContainer projets hidden absolute bottom-10 mx-auto w-full px-4 xs:px-12 sm:px-20 text-lg">
         <div class="swiper swiperProjet relative overflow-visible">
@@ -139,6 +139,14 @@
             </div>
             <?php } ?>
           </div>
+          <div class="ml-auto mr-0">
+            <a href="https://github.com/eliooooooo?tab=repositories" class="buttonPrimary" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+              </svg>
+              <span>Voir tous les projets</span>
+            </a>
+          </div>
         </div>
       </div>
       <div class="contactContainer hidden absolute text-right right-10 bottom-10 mx-auto max-w-[220px]">
@@ -161,7 +169,7 @@
                 </a>
               </li>
             </ul>
-            <span class="block w-fit ml-auto mb-2 border-b-[2px] border-orange-500 dark:border-yellow-400 text-lg">ou par mail :</span>
+            <span class="block w-fit ml-auto mb-2 border-b-[2px] border-orange-500 dark:border-yellow-400 text-xl">ou par mail :</span>
             <span class="text-lg">eliott.burkle@gmail.com</span>
           </div>
           <p class="mt-8 text-lg"><span class="text-orange-500 dark:text-yellow-400">N'hésitez pas</span> à me contacter pour toute opportunité, question ou commentaire. Je serai ravi de vous répondre.</p>
