a9803e66015e5677558b8bd1f2032df47ed21b09
eliooooooo <eliott.burkle@gmail.com>
2025-06-19 12:20:02 +0200

    Fix: two scrollbars

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
       <div class="circle hidden xl:block"></div>
     </div>      
   </div>
-  <main class="container relative mx-auto flex flex-col w-full h-full justify-between pb-20">
+  <main class="container relative mx-auto flex flex-col w-full h-auto justify-between pb-20">
     <div class="themeSwitcher hidden hover:scale-110 transition-all cursor-pointer absolute top-0 right-0 p-2 sm:p-3 z-20 bg-black dark:bg-white text-slate-300 dark:text-blue1 rounded-full m-3 sm:m-5" style="display: none;">
       <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" id="moon" class="bi bi-moon-stars" viewBox="0 0 16 16">
         <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286"/>
